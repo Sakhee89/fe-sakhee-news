4ea4f8356e53fc1be2d61a3f808dc58decfc61d3
Sakhee89 <kung_fu_fighting_monkey@hotmail.com>
2023-12-11 17:48:39 +0000

    add article semantic tag

--- src/components/ArticlesCard.jsx
+++ src/components/ArticlesCard.jsx
@@ -5,22 +5,23 @@ export function ArticlesCard({ article }) {
   const formattedDate = new Date(article.created_at).toLocaleString();
 
   return (
-    <Col className="d-flex" style={{ margin: "10px" }}>
-      <Card className="flex-fill">
-        <Card.Body>
-          <Card.Title>Article Title: {article.title}</Card.Title>
-          <Card.Img variant="top" src={article.article_img_url} />
-
-          <Card.Text>Author: {article.author}</Card.Text>
-          <Card.Text>Topic: {article.topic}</Card.Text>
-          <Card.Text>Votes: {article.votes}</Card.Text>
-          <Card.Text>Comments: {article.comment_count}</Card.Text>
-          <Card.Text>Created_at: {formattedDate}</Card.Text>
-          <Link to={`/articles/${article.article_id}`}>
-            <Button variant="primary">View Article</Button>
-          </Link>
-        </Card.Body>
-      </Card>
-    </Col>
+    <article>
+      <Col className="d-flex" style={{ margin: "10px" }}>
+        <Card className="flex-fill">
+          <Card.Body>
+            <Card.Title>Article Title: {article.title}</Card.Title>
+            <Card.Img variant="top" src={article.article_img_url} />
+            <Card.Text>Author: {article.author}</Card.Text>
+            <Card.Text>Topic: {article.topic}</Card.Text>
+            <Card.Text>Votes: {article.votes}</Card.Text>
+            <Card.Text>Comments: {article.comment_count}</Card.Text>
+            <Card.Text>Created_at: {formattedDate}</Card.Text>
+            <Link to={`/articles/${article.article_id}`}>
+              <Button variant="primary">View Article</Button>
+            </Link>
+          </Card.Body>
+        </Card>
+      </Col>
+    </article>
   );
 }
