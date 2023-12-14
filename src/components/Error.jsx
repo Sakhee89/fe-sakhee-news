2349af2708ef86a678fba5d449350d66ae44f5a1
Sakhee89 <kung_fu_fighting_monkey@hotmail.com>
2023-12-14 16:05:08 +0000

    error handling

---
+++ src/components/Error.jsx
@@ -0,0 +1,8 @@
+export function Error({ message }) {
+  return (
+    <div>
+      <h2>Error!</h2>
+      <p>{message}</p>
+    </div>
+  );
+}
--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import { getArticles, getTopics } from "../utils/utils";
 import { Link, useParams } from "react-router-dom";
 import { ArticlesCard } from "./ArticlesCard";
+import { Error } from "./Error";
 
 export function Topics({ sortbyQuery, orderQuery }) {
   const [topics, setTopics] = useState([]);
   const { topic } = useParams();
   const [loading, setLoading] = useState(false);
   const [articles, setArticles] = useState([]);
+  const [apiError, setApiError] = useState(null);
 
   useEffect(() => {
     getTopics().then((res) => {
@@ -19,15 +21,26 @@ export function Topics({ sortbyQuery, orderQuery }) {
   useEffect(() => {
     setLoading(true);
     if (topic) {
-      getArticles(topic, sortbyQuery, orderQuery).then((res) => {
-        setLoading(false);
-        setArticles(res.data.articles);
-      });
+      getArticles(topic, sortbyQuery, orderQuery)
+        .then((res) => {
+          setArticles(res.data.articles);
+          setLoading(false);
+        })
+        .catch((err) => {
+          if (err.response.status === 400) {
+            setApiError("Bad Request");
+          } else {
+            setApiError("Topic does not exist");
+          }
+          setLoading(false);
+        });
     }
   }, [topic]);
 
   if (loading) {
     return <p>Loading...</p>;
+  } else if (apiError) {
+    return <Error message={apiError} />;
   }
 
   return (
